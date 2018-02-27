8b26eefec9af0d23dd7793af325ca1363654c976
cjpennock <cjpennock@gmail.com>
2018-02-27 16:16:12 -0500

    first commit

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './BusinessList.css';
+// import <Business /> component - we will use it in the BusinessList component
+import Business from '../Business/business.js';
+
+class BusinessList extends React.Component {
+	render() {
+		return (
+			// Ask somebody about this callback
+			<div className="BusinessList">
+			  {this.props.businesses.map(business => {
+			  	return <Business business={business} />
+			  }
+			  	)}
+			</div>
+			);
+	}
+};
+
+export default BusinessList;
